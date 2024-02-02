3a6b528b653c4ab24bb6e5a96a8d2005276afe8f
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-02-02 15:03:55 +0200

    omg

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNum = document.querySelector("#controls input")
+const createBtn = document.querySelector("#controls button[data-create]")
+const destrBtn = document.querySelector("#controls button[data-destroy]")
+const boxCollection = document.querySelector("#boxes")
+const randomBox = document.createElement("div")
+randomBox.style.width = "300px"
+randomBox.style.heigh = "300px"
+randomBox.style.backgroundColor = '${ getRandomHexColor() }'
+randomBox.style.marginLeft = "auto"
+randomBox.style.marginRight = "auto"
+
+createBtn.addEventListener("click", addItems)
+destrBtn.addEventListener("click", deleteItems)
+
+function addItems(event) {
+  return boxCollection.insertAdjacentHTML('beforeend', randomBox);
+}
