2f0db35319faa78236f4fa0dab55cead8bf522ac
Hrechaniuk1 <hrechaniuk.a@gmail.com>
2024-02-03 16:34:49 +0200

    hw -7-done

--- js/task-6.js
+++ js/task-6.js
@@ -8,35 +8,38 @@ const inputNum = document.querySelector("#controls input")
 const createBtn = document.querySelector("#controls button[data-create]")
 const destrBtn = document.querySelector("#controls button[data-destroy]")
 const boxCollection = document.querySelector("#boxes")
-const randomBox = `<div style=" width: ${getSizeOfBox()}px; height: ${getSizeOfBox()}px; margin-left: auto; margin-right: auto; margin-bottom: 30px; background-color: ${getRandomHexColor() } ;"></div>`
 
-function getSizeOfBox() {
-  let sizeOf = 100
-  for (let i = 1; i <= getInputValue(); i++){
-sizeOf += 30
-
-  }
-  return sizeOf
-} 
-
-createBtn.addEventListener("click", addItems)
-destrBtn.addEventListener("click", deleteItems)
+createBtn.addEventListener("click", createBoxes)
+destrBtn.addEventListener("click", destroyBoxes)
 inputNum.addEventListener("change", getInputValue)
 
-function addItems(event) {
-  for (let i = 1; i <= getInputValue(); i++)
-    boxCollection.insertAdjacentHTML('beforeend', randomBox);
-  inputNum.reset()
+
+let boxSize = 30
+  
+
+function createBoxes() {
+  if ((getInputValue() <= 100) && (getInputValue() >= 1)) {
+
+    for (let i = 1; i <= getInputValue(); i++) {
+      const randomBox = `<div style=" width: ${boxSize}px; height: ${boxSize}px; background-color: ${getRandomHexColor()} ;"></div>`
+      boxCollection.insertAdjacentHTML('beforeend', randomBox);
+      boxSize += 10
+    }
+  }
+  inputNum.value = ''
   return
 }
 
 
-function getInputValue(event) {
+function getInputValue() {
   let amount = inputNum.value;
   return amount
 
 }
 
-function deleteItems(event) {
-  return boxCollection.innerHTML = ""
+function destroyBoxes() {
+  boxCollection.innerHTML = ""
+  boxSize = 30
+  return
 }
+
